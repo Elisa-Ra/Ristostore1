292b761ef20924e0c1fe1fdf9937a8bf135f7690
S <380gradi@gmail.com>
2022-04-30 18:16:12 +0200

    primo commit

--- App.js
+++ App.js
@@ -1,12 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Linking, Image,ScrollView,SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import Constants from 'expo-constants';
+// import { Card } from 'react-native-paper';
+import { Dimensions } from 'react-native';
+
+
+const Stack = createStackNavigator();
+const win = Dimensions.get('window');
+const ratio = win.width / 434;
+
+function ScreenA({navigation}){ //pagina iniziale
+  const screenb= () => {
+    navigation.navigate('Ristostore');
+  }
+  const screenc= () => {
+    navigation.navigate('Area Drivers');
+  }
+  const screend= () => {
+    navigation.navigate('Area Gestori');
+  }
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+
+        <View style={styles.container}> 
+          <View style={styles.div}>
+            <TouchableWithoutFeedback onPress={screenb}>
+              <Image source={require('./assets/entra.jpg')}  
+              style={{
+                width: win.width,
+                height: 217 * ratio,
+              }}/>
+          </TouchableWithoutFeedback>
+        </View>
+        <Text style={styles.text}>Prova la nostra app</Text>
+        <View style={styles.div}>
+          <TouchableWithoutFeedback onPress={screenc}>
+            <Image source={require('./assets/rider.jpg')}  
+            style={{
+              width: win.width,
+              height: 217 * ratio,
+            }}
+            />
+          </TouchableWithoutFeedback> 
+          
+        </View>
+        <Text style={styles.text}>Area Driver</Text>
+        <View style={styles.div}>
+          <TouchableWithoutFeedback onPress={screend}>
+            <Image source={require('./assets/gestori.jpg')} 
+            style={{
+              width: win.width,
+              height: 217 * ratio,
+            }}
+            />
+          </TouchableWithoutFeedback> 
+        
+        </View>
+        <Text style={styles.text}>Area Gestori</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+function ScreenB(){ //ristostore home
+  return (
+   
+      <WebView name='1'
+        source={{ uri: 'https://ristostore.it/' }} 
+      />
+  
+  );
+}
+function ScreenC(){ //ristostore driver
+  return (
+    
+      <WebView name='2'
+        source={{ uri: 'https://ristostore.it/Area_Drivers/accesso' }} 
+      />
+   
+  );
+}
+function ScreenD(){ //ristostore gestori
+  return (
+    
+      <WebView name='3'
+        source={{ uri: 'https://ristostore.it/Area_Gestori/accesso' }} 
+      />
+    
+  );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Ristostore App" component = {ScreenA}/>
+          <Stack.Screen name="Ristostore" component = {ScreenB} />
+          <Stack.Screen name="Area Drivers" component = {ScreenC}/>
+          <Stack.Screen name="Area Gestori" component = {ScreenD}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -17,4 +121,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  div:{
+    flex:1,
+    width:'100%',
+    height:'100%'
+  },
+  img:{
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
+    
+  }, 
+  text:{
+    textAlign:'center',
+    color:'#17a2b8',
+    fontFamily:'serif',
+    fontSize:20
+  },
+  text1:{
+    textAlign:'center',
+    color:'#17a2b8',
+    fontSize:20,
+    fontFamily:'serif'
+  }
+
 });
+
