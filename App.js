2cf009d6d7d71607dce9a7e56b6fe5f2c8714e3e
S <380gradi@gmail.com>
2022-11-02 20:25:31 +0100

    nuovo vecchio layout

--- App.js
+++ App.js
@@ -1,115 +1,156 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View,ScrollView,SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View,ScrollView,SafeAreaView,Image,Text } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { createStackNavigator } from '@react-navigation/stack';
 // import 'react-native-gesture-handler';
-// import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 // import Constants from 'expo-constants';
 // import { Card } from 'react-native-paper';, useEffect 
-// import { Dimensions } from 'react-native';
-// import React, { useRef} from 'react';
-import React from 'react';
+import { Animated, Dimensions } from 'react-native';
+import React, { useRef} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+// import React from 'react';
 
 
-// const Stack = createStackNavigator();
-// const win = Dimensions.get('window');
-// const ratio = win.width / 434;
-// const FadeInView = (props) => {
-//   const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+const Stack = createStackNavigator();
+const win = Dimensions.get('window');
+const ratio = win.width / 434;
+const FadeInView = (props) => {
+const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
 
-//   React.useEffect(() => {
-//     Animated.timing(
-//       fadeAnim,
-//       {
-//         toValue: 1,
-//         duration: 800,
-//         useNativeDriver: true,
-//         delay:props.ritardo
-//       }
-//     ).start();
-//   }, [fadeAnim])
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 800,
+        useNativeDriver: true,
+        delay:props.ritardo
+      }
+    ).start();
+  }, [fadeAnim])
 
-//   return (
-//     <Animated.View                 // Special animatable View
-//       style={{
-//         ...props.style,
-//         opacity: fadeAnim,         // Bind opacity to animated value
-//       }}
-//     >
-//       {props.children}
-//     </Animated.View>
-//   );
-// }
-
-
-// function ScreenA({navigation}){ //pagina iniziale
-//   // const screenb= () => {
-//   //   navigation.navigate('Ristostore');
-//   // }
-//   // const screenc= () => {
-//   //   navigation.navigate('Area Drivers');
-//   // }
-//   // const screend= () => {
-//   //   navigation.navigate('Area Gestori');
-//   // }
+  return (
+    <Animated.View                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
 
 
+function ScreenA({navigation}){ //pagina iniziale
+  const screenb= () => {
+    navigation.navigate('Ristostore');
+  }
+  // const screenc= () => {
+  //   navigation.navigate('Area Drivers');
+  // }
+  // const screend= () => {
+  //   navigation.navigate('Area Gestori');
+  // }
 
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <ScrollView>
 
-//         <View style={styles.container}> 
-//           {/* <FadeInView  style={styles.div} ritardo={400}>
-//             <TouchableWithoutFeedback onPress={screenb}>
-//               <Image source={require('./assets/entra.jpg')}  
-//               style={{
-//                 width: win.width,
-//                 height: 217 * ratio,
-//               }}/>
-//           </TouchableWithoutFeedback>
-//           <Text style={styles.text}>Prova la nostra app</Text>
-//           </FadeInView>
-        
-//           <FadeInView style={styles.div}  ritardo={800}>
-//             <TouchableWithoutFeedback onPress={screenc}>
-//               <Image source={require('./assets/rider.jpg')}  
-//               style={{
-//                 width: win.width,
-//                 height: 217 * ratio,
-//               }}
-//               />
-//             </TouchableWithoutFeedback> 
-//             <Text style={styles.text}>Area Driver</Text>
-//           </FadeInView>
-        
-//           <FadeInView style={styles.div} ritardo={1200}>
-//             <TouchableWithoutFeedback onPress={screend}>
-//               <Image source={require('./assets/gestori.jpg')} 
-//               style={{
-//                 width: win.width,
-//                 height: 217 * ratio,
-//               }}
-//               />
-//             </TouchableWithoutFeedback> 
-//           <Text style={styles.text}>Area Gestori</Text>
-//           </FadeInView> */}
-//           {/* <WebView name='1' style={styles.div}
-//             source={{ uri: 'https://ristostore.it/' }} 
-//           /> */}
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-// function ScreenB(){ //ristostore home
-//   return (
-//       <WebView name='1'
-//         source={{ uri: 'https://ristostore.it/' }} 
-//       />
-  
-//   );
-// }
+
+  return (
+    <SafeAreaView style={[{backgroundColor:'#01a1ad', paddingBottom:16}]}>
+      <ScrollView>
+
+          <View style={[{backgroundColor:"#fff", width:"100%", alignItems:'center', paddingTop:24,paddingBottom:12}]}> 
+            <Image source={require('./assets/logoorizzontale.jpg')}  
+              style={{
+                width: 250,
+                height: 68
+              }}
+            />     
+          </View>
+          <View style={styles.container}> 
+              <Text  style={[{color:'#fff', fontSize:24, margin:8, textAlign:'center'}]}>Ciao, sei su Ristostore, cosa posso portarti oggi?</Text>
+          </View>
+          <View style={styles.container}> 
+            <View style={styles.container}> 
+              <FadeInView  style={styles.div} ritardo={400}>
+                <TouchableWithoutFeedback onPress={screenb}>
+                  <Image source={require('./assets/cibo.png')}  
+                    style={{
+                      width: 200,
+                      height: 182,
+                      // width: win.width,
+                      // height: 217 * ratio,
+                    }}
+                  />
+                </TouchableWithoutFeedback>
+              </FadeInView>
+              <FadeInView  style={styles.div} ritardo={800}>
+                <TouchableWithoutFeedback onPress={screenb}>
+                  <Image source={require('./assets/spesa.png')}  
+                    style={{
+                      width: 200,
+                      height: 182,
+                      // width: win.width,
+                      // height: 217 * ratio,
+                    }}
+                  />
+                </TouchableWithoutFeedback>
+              </FadeInView>
+              <FadeInView  style={styles.div} ritardo={1200}>
+                <TouchableWithoutFeedback onPress={screenb}>
+                  <Image source={require('./assets/negozi.png')}  
+                    style={{
+                      width: 200,
+                      height: 182,
+                      // width: win.width,
+                      // height: 217 * ratio,
+                    }}
+                  />
+                </TouchableWithoutFeedback>
+              </FadeInView>
+            {/*
+              <FadeInView style={styles.div}  ritardo={800}>
+                <TouchableWithoutFeedback onPress={screenc}>
+                  <Image source={require('./assets/rider.jpg')}  
+                  style={{
+                    width: win.width,
+                    height: 217 * ratio,
+                  }}
+                  />
+                </TouchableWithoutFeedback> 
+                <Text style={styles.text}>Area Driver</Text>
+              </FadeInView>
+            
+              <FadeInView style={styles.div} ritardo={1200}>
+                <TouchableWithoutFeedback onPress={screend}>
+                  <Image source={require('./assets/gestori.jpg')} 
+                  style={{
+                    width: win.width,
+                    height: 217 * ratio,
+                  }}
+                  />
+                </TouchableWithoutFeedback> 
+              <Text style={styles.text}>Area Gestori</Text>
+              </FadeInView> */}
+              {/* <WebView name='1' style={styles.div}
+                source={{ uri: 'https://ristostore.it/' }} 
+              /> */}
+            </View>          
+        </View>
+        <StatusBar hidden={true} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+function ScreenB(){ //ristostore home
+  return (
+    <>
+      <WebView name='1' source={{ uri: 'https://ristostore.it/' }}  />
+      <StatusBar hidden={true} />
+    </>
+  );
+}
 // function ScreenC(){ //ristostore driver
 //   return (
     
@@ -131,24 +172,23 @@ import React from 'react';
 
 export default function App() {
     return (
-      <WebView name='1'
-        source={{ uri: 'https://ristostore.it/' }} style={styles.mt}
-      />
-      // <NavigationContainer>
-      //   <Stack.Navigator>
-      //     <Stack.Screen name="Ristostore App" component = {ScreenA}/>
-      //     {/* <Stack.Screen name="Ristostore" component = {ScreenB} /> */}
-      //     {/* <Stack.Screen name="Area Drivers" component = {ScreenC}/>
-      //     <Stack.Screen name="Area Gestori" component = {ScreenD}/> */}
-      //   </Stack.Navigator>
-      // </NavigationContainer>
+      // <WebView name='1'
+      //   source={{ uri: 'https://ristostore.it/' }} style={styles.mt}
+      // />
+      <NavigationContainer>
+        <Stack.Navigator  screenOptions={{    headerShown: false  }}>
+          <Stack.Screen name="Ristostore App" component = {ScreenA}/>
+          <Stack.Screen name="Ristostore" component = {ScreenB} />
+          {/* <Stack.Screen name="Area Drivers" component = {ScreenC}/>
+          <Stack.Screen name="Area Gestori" component = {ScreenD}/> */}
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
